d5e892a3e6ea908a1c17207d809bf0234ada9c02
Avighnash <avighnash@gmail.com>
2016-07-27 12:47:12 -0700

    Update GUIInteractionListener.java

--- src/main/java/us/universalpvp/kit/event/listeners/GUIInteractionListener.java
+++ src/main/java/us/universalpvp/kit/event/listeners/GUIInteractionListener.java
@@ -1,11 +1,13 @@
 package us.universalpvp.kit.event.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import us.universalpvp.kit.KitMain;
 import us.universalpvp.kit.api.KitAPI;
+import us.universalpvp.kit.event.events.KitRecieveEvent;
 
 /**
  * Created by avigh on 7/24/2016.
@@ -26,20 +28,18 @@ public class GUIInteractionListener implements Listener {
                 getConfigurationSection("Gui").getString("name"))) {
 
             KitAPI.getAPI().getRegisteredKits().stream().filter(k -> k.getGuiItem().isSimilar(e.getCurrentItem())).forEach(k -> {
-                if (k.isPermissible() && p.hasPermission("kits." + k.getName().toLowerCase())) {
+                if (k.isPermissible() && p.hasPermission("kit." + k.getName().toLowerCase())) {
                     p.getInventory().addItem(k.getItems());
                     p.getInventory().setArmorContents(k.getArmor());
+
                 } else {
                     p.getInventory().addItem(k.getItems());
                     p.getInventory().setArmorContents(k.getArmor());
                 }
-
+                
+                Bukkit.getPluginManager().callEvent(new KitRecieveEvent(p, k));
                 p.sendMessage(plugin.getConfig().getString("Kit-received"));
             });
-
         }
-
     }
-
-
 }
