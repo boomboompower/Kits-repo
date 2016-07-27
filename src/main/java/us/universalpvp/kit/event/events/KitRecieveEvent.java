e29044068c46176fc7fdc6a174134712f30ba9b6
Avighnash <avighnash@gmail.com>
2016-07-27 12:48:53 -0700

    Update KitRecieveEvent.java

--- src/main/java/us/universalpvp/kit/event/events/KitRecieveEvent.java
+++ src/main/java/us/universalpvp/kit/event/events/KitRecieveEvent.java
@@ -1,7 +1,6 @@
 package us.universalpvp.kit.event.events;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import us.universalpvp.kit.api.Kit;
@@ -9,13 +8,13 @@ import us.universalpvp.kit.api.Kit;
 /**
  * Created by avigh on 7/24/2016.
  */
-public class KitRecieveEvent extends Event implements Cancellable {
+public class KitReceiveEvent extends Event {
 
     private static final HandlerList HANDLERS = new HandlerList();
     private Player receiver;
     private Kit kit;
 
-    public KitRecieveEvent(Player receiver, Kit kit) {
+    public KitReceiveEvent(Player receiver, Kit kit) {
         this.receiver = receiver;
         this.kit = kit;
     }
@@ -31,15 +30,5 @@ public class KitRecieveEvent extends Event implements Cancellable {
     @Override
     public HandlerList getHandlers() {
         return HANDLERS;
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return false;
-    }
-
-    @Override
-    public void setCancelled(boolean b) {
-
     }
 }
